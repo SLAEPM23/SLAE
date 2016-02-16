c67c2b878c28da9f989d791fdd902bdd496faf48
Igor542 <safonrf1@mail.ru>
2016-02-17 00:09:54 +0600

    Add Interface and realization Solver

---
+++ slae/slae/Interface/ISolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slae.Interface
+{
+    interface ISolver
+    {
+        IVector Solve(IMatrix matrix, IVector rp, double MinResidual = 1e-20, int MaxIter = 10000);
+        double Residual { get; }
+        int Iter { get; }
+    }
+}
