79101eb06511278ad65ed4359760809340ac65e2
Qireel <kirill.karmachev@gmail.com>
2016-02-20 17:55:02 +0600

    Сделали всё что нужно для реализации метода Якоби
    работаем!

--- slae/slae/Matrix/Matrix.cs
+++ slae/slae/Matrix/Matrix.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace slae
 {
-    class Matrix: Interface.IMatrix
+    class RowColumnSparseMatrix : Interface.IMatrix
     {
+        int[] ia;
+        int[] ja;
+        Vector al;
+        Vector di;
+        int size;
+        RowColumnSparseMatrix(int sz, int[] _ia, int[] _ja,
+            double[] _al, double[] _di)
+        {
+            size = sz;
+            ia = _ia; ja = _ja; al = new Vector(_al); di = new Vector(_di);
+        }
+
+        public void Run(Interface.ProcessElement processor)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                processor(i, i, di[i]);
+                for (int jaddr = ia[i]; jaddr < ia[i + 1]; jaddr++)
+                {
+                    processor(i, ja[jaddr], al[jaddr]);
+                    processor(ja[jaddr], i, al[jaddr]);
+                }
+            }
+        }
+
+        public Interface.IVector Diagonal
+        {
+            get { return di; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
     }
 }
---
+++ slae/slae/Matrix/MatrixAssistant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slae
+{
+    public static class MatrixAssistant
+    {
+        static Interface.IVector multMatrixVector(Interface.IMatrix matr, Interface.IVector vec)
+        {
+            Interface.IVector res = vec.Clone() as Interface.IVector;
+            res.Nullify();
+            matr.Run
+                (
+                (int i, int j, double el) => { res[j] += el * vec[j]; }
+                );
+            return res;
+        }
+    }
+}
