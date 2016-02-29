a2c7077491521f9397c7f1a7cd1cba088fe6fe8d
unknown <malardyrova.2012@pmi-307-10.corp.nstu.ru>
2016-02-29 11:58:05 +0600

    exceptions added

--- slae/slae/Program.cs
+++ slae/slae/Program.cs
@@ -16,23 +16,30 @@ namespace slae
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Form1 Forma = new Form1();
-            Application.Run(Forma);
+            try
+            {
+                Form1 Forma = new Form1();
+                Application.Run(Forma);
 
-            string MethodKeeping = Forma.meth_keep;//метод хранения
-            string MethodSolving = Forma.meth_solv;//метод решения
-            string Path = Forma.path;//файл с исходными данными
+                string MethodKeeping = Forma.meth_keep;//метод хранения
+                string MethodSolving = Forma.meth_solv;//метод решения
+                string Path = Forma.path;//файл с исходными данными
 
-            RowColumnSparseMatrix Matrix;
-            Vector Rp;
-            Vector x0;
+                RowColumnSparseMatrix Matrix;
+                Vector Rp;
+                Vector x0;
 
-            Input.input Inp = new Input.input(Path);
-            Inp.ReadFromFile(out Matrix, out Rp, out x0);
+                Input.input Inp = new Input.input(Path);
+                Inp.ReadFromFile(out Matrix, out Rp, out x0);
 
-            
-            Jacobi Solving = new Jacobi();
-            Vector solution = (Vector)Solving.Solve(Matrix, Rp, x0);
+
+                Jacobi Solving = new Jacobi();
+                Vector solution = (Vector)Solving.Solve(Matrix, Rp, x0);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
