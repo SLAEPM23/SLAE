3b1abff444d0d3e9ffab8603ac3f6fc68961fd4e
ImpossibleProg <programm13@gmail.com>
2016-03-23 13:02:43 +0600

    попытка-не пытка (отрицательный лос)

--- slae/slae/Solver/LOS.cs
+++ slae/slae/Solver/LOS.cs
@@ -44,7 +44,10 @@ namespace slae
                 p = VectorAssistant.multScalar(betta, p);
                 p.Add(MatrixAssistant.multMatrixVector(A,r),1);
                 residual_prev = residual;
-                residual -= alpha*alpha*p_scal;
+                residual -= alpha*alpha*p_scal;  // очень смущает, - как раз тут вылезает всегда на последней итериции
+
+               if (residual < 0) residual = residual_prev; //ну не знаю что сделать (пока что), пусть будет так
+
                 //Debugger.DebugSolver(iteration, residual, result);
             }
             return result;
