3ab9044bf1e0c925e84664d377e8cba604b0f7bf
ImpossibleProg <programm13@gmail.com>
2016-03-28 15:21:03 +0600

    невязка лося, Тодышев!

--- slae/slae/Solver/LOS.cs
+++ slae/slae/Solver/LOS.cs
@@ -10,7 +10,7 @@ namespace slae
     class LOS : Solver
     {
         public LOS(int _maxIteration = 10000, double _minResidual = 1E-16)
-        { 
+        {
             maxIteration = _maxIteration;
             minResidual = _minResidual;
         }
@@ -18,37 +18,35 @@ namespace slae
         public override IVector Solve(IMatrix A, IVector b, IVector x0)
         {
             IVector result = new Vector(b.Size);
-            IVector r= new Vector(b.Size);
+            IVector r = new Vector(b.Size);
             IVector p = new Vector(b.Size);
             IVector z = new Vector(b.Size);
             double alpha, betta;
             double p_scal;
-            double residual_prev;//для проверки изменения невязки
 
             r.Equalize(b);
             result.Equalize(x0);
-            r.Add(MatrixAssistant.multMatrixVector(A,result),-1);
+            r.Add(MatrixAssistant.multMatrixVector(A, result), -1);
             z.Equalize(r);
-            p.Equalize(MatrixAssistant.multMatrixVector(A,z));
+            p.Equalize(MatrixAssistant.multMatrixVector(A, z));
+            p_scal = VectorAssistant.multVector(p, p);
             residual = VectorAssistant.multVector(r, r);
-            residual_prev = 2*residual;
-            for (iteration = 1; iteration <= maxIteration && residual > minResidual && residual_prev - residual > minResidual; iteration++)
+
+            for (iteration = 1; iteration <= maxIteration && residual > minResidual; iteration++)
             {
-                p_scal = VectorAssistant.multVector(p, p);
-                alpha = VectorAssistant.multVector(p,r)/p_scal;
-                result.Add(z,alpha);
-                r.Add(p,-alpha);
-                betta = -VectorAssistant.multVector(p, MatrixAssistant.multMatrixVector(A, r))/p_scal;
+                alpha = VectorAssistant.multVector(p, r) / p_scal;
+                result.Add(z, alpha);
+                r.Add(p, -alpha);
+                betta = -VectorAssistant.multVector(p, MatrixAssistant.multMatrixVector(A, r)) / p_scal;
                 z = VectorAssistant.multScalar(betta, z);
-                z.Add(r,1);
+                z.Add(r, 1);
                 p = VectorAssistant.multScalar(betta, p);
-                p.Add(MatrixAssistant.multMatrixVector(A,r),1);
-                residual_prev = residual;
-                residual -= alpha*alpha*p_scal;  
+                p.Add(MatrixAssistant.multMatrixVector(A, r), 1);
 
-               if (residual < 0) residual = residual_prev; //ну не знаю что сделать (пока что), пусть будет так
-
-                //Debugger.DebugSolver(iteration, residual, result);
+                residual -= alpha * alpha * p_scal;
+                if (alpha * alpha * p_scal < residual * 10E-5)
+                    return result;
+                p_scal = VectorAssistant.multVector(p, p);
             }
             return result;
         }
